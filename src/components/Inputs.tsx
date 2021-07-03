b38f5e180cc4d54eb1ab2347682373306f42e3ef
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-07-03 23:56:02 +0200

    Added inputs fields

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import intervals from '../intervals';
 
 const Inputs = () => {
-	return <div>Inputs</div>;
+	return (
+		<form>
+			<input type='number' name='' id='' />
+			<input type='number' name='' id='' />
+			<select name='' id=''>
+				{intervals.map((interval) => (
+					<option value={interval.value}>
+						{interval.description}
+					</option>
+				))}
+			</select>
+			<input type='number' name='' id='' />
+			<input type='number' name='' id='' />
+		</form>
+	);
 };
 
 export default Inputs;
