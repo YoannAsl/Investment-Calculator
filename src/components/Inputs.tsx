44a5b441879799214e5ccd874872e006d70395bb
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-07-19 22:21:52 +0200

    Added controlled inputs for form

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import intervals from '../intervals';
 
@@ -10,26 +10,63 @@ const Form = styled.form`
 `;
 
 const Inputs = () => {
+	const [inputs, setInputs] = useState({
+		startingAmount: 0,
+		contribution: 0,
+		interval: 0,
+		return: 0,
+		period: 0,
+	});
+
+	const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+		const target = e.currentTarget;
+		const { name, value } = target;
+		// const value = +target.value;
+		setInputs({ ...inputs, [name]: +value });
+	};
+
+	console.log(inputs);
+
 	return (
 		<Form>
-			<label htmlFor='starting-amout'>Starting Amount</label>
-			<input type='number' name='starting-amount' />
+			<label htmlFor='startingAmount'>Starting Amount</label>
+			<input
+				type='number'
+				name='startingAmount'
+				onChange={handleChange}
+				value={inputs.startingAmount}
+			/>
 
 			<label htmlFor='contribution'>Additionnal Contribution</label>
-			<input type='number' name='contribution' />
+			<input
+				type='number'
+				name='contribution'
+				onChange={handleChange}
+				value={inputs.contribution}
+			/>
 			<select name='interval'>
 				{intervals.map((interval) => (
-					<option value={interval.value}>
+					<option key={interval.value} value={interval.value}>
 						{interval.description}
 					</option>
 				))}
 			</select>
 
 			<label htmlFor='return'>Rate of Return</label>
-			<input type='number' name='return' />
+			<input
+				type='number'
+				name='return'
+				onChange={handleChange}
+				value={inputs.return}
+			/>
 
 			<label htmlFor='period'>Years to Grow</label>
-			<input type='number' name='period' />
+			<input
+				type='number'
+				name='period'
+				onChange={handleChange}
+				value={inputs.period}
+			/>
 		</Form>
 	);
 };
